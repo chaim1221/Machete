24f41dddae383f7f363d3a4a1bf92034f3c1feb1
Chaim Eliyah <chaimeliyah@gmail.com>
2019-01-15 12:07:58 -0800

    remove unused usings

--- Machete.Data/MacheteAdoContext.cs
+++ Machete.Data/MacheteAdoContext.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
-using Castle.Core.Internal;
 using Machete.Data.Dynamic;
 using Newtonsoft.Json;
 
