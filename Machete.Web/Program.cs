fec8d64b2aae164966a8d882779d5835f47bf988
Chaim Eliyah <chaimeliyah@gmail.com>
2019-02-03 13:13:24 -0800

    fix console logging for CustomWebHostBuilder (was CreateWebHostBuilder)

--- Machete.Web/Program.cs
+++ Machete.Web/Program.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace Machete.Web
 {
@@ -9,19 +10,26 @@ namespace Machete.Web
         /// <summary>
         /// The program's Main method; entry point for the application.
         /// </summary>
-        public static void Main(string[] args) => CreateWebHostBuilder(args).Build().Run();        
+        public static void Main(string[] args) => CustomWebHostBuilder(args).Build().Run();        
 
         /// <summary>
         /// A stripped down version of the default WebHost object configuration, this method gives just the absolute
         /// basics necessary to run an MVC app on POSIX. It does not contain configuration for IIS, instead using only
         /// the Kestrel development server; ideal for hosting with containers.
         /// </summary>
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHostBuilder CustomWebHostBuilder(string[] args) =>
             new WebHostBuilder()
                 .UseKestrel()
+              //.UseContentRoute() //uncomment for static content route
                 .ConfigureAppConfiguration((host, config) => {
                     config.SetBasePath(Directory.GetCurrentDirectory());
                     config.AddJsonFile("appsettings.json");
+                })
+                .ConfigureLogging((app, logging) => {
+                    logging.AddConfiguration(app.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddEventSourceLogger();
                 })
                 .UseStartup<Startup>();
     }
