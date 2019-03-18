c58effbe7758a86ca5ffd9cf40e1c8b76d9b44a7
Chaim <chaimeliyah@gmail.com>
2019-03-17 21:41:47 -0700

    update UI

--- dockerize.sh
+++ dockerize.sh
@@ -29,7 +29,7 @@ docker build -t machete/debian:1.15.0 .
 
 echo "to run, type:"
 echo ""
-echo "docker run -dit --name machete1 --network machete-bridge -p 4213:4213 -p 80:80 machete/debian:1.15.0"
+echo "docker run -dit --name machete1 --network machete-bridge -p 443:443 -p 80:80 machete/debian:1.15.0"
 # echo "docker run --rm -it --network host -e ASPNETCORE_URLS=\"https://+\" -e ASPNETCORE_HTTPS_PORT=4213 -e ASPNETCORE_Kestrel__Certificates__Default__Path=/https/aspnetapp.pfx -v ${HOME}/.aspnet/https:/https/ --name machete1 machetecontainer"
 echo ""
 echo "or something like that."
