006ede4388c5b3c5d4fe0a39fceb8c3c0eec1104
Chaim <chaimeliyah@gmail.com>
2019-05-22 10:10:26 -0700

    create our migration on top of classic

--- Machete.Data/Migrations/20190522164436_InitialCreate.Designer.cs
+++ Machete.Data/Migrations/20190522164436_InitialCreate.Designer.cs
@@ -4,14 +4,16 @@ using Machete.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Machete.Data.Migrations
 {
     [DbContext(typeof(MacheteContext))]
-    partial class MacheteContextModelSnapshot : ModelSnapshot
+    [Migration("20190522164436_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
